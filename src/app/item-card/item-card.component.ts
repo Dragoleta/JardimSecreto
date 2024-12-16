49fb487e80f954f4654c8ba89a15972cf17dcf1d
Jorge Dantas <jorge@pop-os.Dlink>
2024-12-16 20:27:53 -0300

    Items list styling changes

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -1,28 +1,32 @@
-import { Component, Input } from '@angular/core';
-import { ShoppingCartService } from '../shopping-cart.service';
-import { ShoppingItem } from './shopping-item';
+import { Component, Input } from "@angular/core";
+import { ShoppingCartService } from "../shopping-cart.service";
+import { ShoppingItem } from "./shopping-item";
 
 @Component({
-    selector: 'app-item-card',
-    standalone: true,
-    imports: [],
-    template: `
-    <div class="card-wrapper">
-        <img src="{{item.imageUrl}}" alt="">
+	selector: "app-item-card",
+	standalone: true,
+	imports: [],
+	template: `
+		<div class="card-wrapper">
+			<a (click)="shoppingCartService.addItem(item!)">
+				<!-- TODO: Change from intead of (A) adding, redir to the details page -->
+				<img src="{{ item.imageUrl }}" alt="" />
 
-        <div class="card-info">
-            <h2>{{item.name}}</h2>
-            <p>R$ {{item.price}}</p>
-            <button (click)="shoppingCartService.addItem(item)">Buy</button>
-        </div>
-    </div>
-  `,
-    styleUrl: './item-card.component.css'
+				<div class="card-info">
+					<div class="item-info">
+						<h2>{{ item.name }}</h2>
+						<p>R$ {{ item.price }}</p>
+					</div>
+					<!-- <button (click)="shoppingCartService.addItem(item)">
+						Buy
+					</button> -->
+				</div>
+			</a>
+		</div>
+	`,
+	styleUrl: "./item-card.component.css",
 })
 export class ItemCardComponent {
-    @Input() item!: ShoppingItem;
-    constructor(public shoppingCartService: ShoppingCartService) {
-        console.log(this.item)
-
-    }
+	@Input() item!: ShoppingItem;
+	constructor(public shoppingCartService: ShoppingCartService) {}
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,43 +1,44 @@
-import { computed, Injectable, signal, WritableSignal } from '@angular/core';
-import { ShoppingItem } from './item-card/shopping-item';
+import { computed, Injectable, signal, WritableSignal } from "@angular/core";
+import { ShoppingItem } from "./item-card/shopping-item";
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: "root",
 })
 export class ShoppingCartService {
-    showCart = signal(false);
-    itemList: WritableSignal<ShoppingItem[]> = signal([]);
+	showCart = signal(false);
+	itemList: WritableSignal<ShoppingItem[]> = signal([]);
 
-    totalValue = computed(() => {
-        if (this.itemList().length === 0) return 0.00
-        return this.itemList().reduce((sum, item) => sum + item.price, 0)
+	totalValue = computed(() => {
+		if (this.itemList().length === 0) return 0.0;
+		return this.itemList().reduce((sum, item) => sum + item.price, 0);
+	});
 
-    }
-    )
+	constructor() {}
 
-    constructor() { }
+	addItem(item: ShoppingItem) {
+		console.log("adding");
+		this.itemList.update((values) => {
+			if (values.some((existingItem) => existingItem.id === item.id)) {
+				item.addOne();
+				return values;
+			}
+			return [...values, item];
+		});
+	}
 
-    addItem(item: ShoppingItem) {
-        this.itemList.update(values => {
-            if (values.some(existingItem => existingItem.id === item.id)) {
-                item.addOne()
-                return values;
-            }
-            return [...values, item]
-        });
-    }
+	removeItem(item: ShoppingItem) {
+		item.quantity = 0;
+		this.itemList.update((values) =>
+			values.filter((i) => i.id !== item.id)
+		);
+	}
 
-    removeItem(item: ShoppingItem) {
-        item.quantity = 0;
-        this.itemList.update(values => values.filter(i => i.id !== item.id));
-    }
+	displayCart() {
+		this.showCart.set(true);
+		console.log(this.showCart());
+	}
 
-    displayCart() {
-        this.showCart.set(true);
-        console.log(this.showCart());
-    }
-
-    closeCart() {
-        this.showCart.set(false);
-    }
+	closeCart() {
+		this.showCart.set(false);
+	}
 }
